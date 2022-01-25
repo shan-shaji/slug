e24d20e73c111ac382b49315a5245dc2231af221
shan-shaji <abrahamshanshaji@gmail.com>
2022-01-25 13:55:23 +0530

    feat: add spinner options

--- example/main.dart
+++ example/main.dart
@@ -2,16 +2,12 @@ import 'dart:async';
 import 'package:slug/slug.dart';
 
 Future<void> main(List<String> args) async {
-  var logger =  Slug.standard(ansi: Ansi(true));
+  var logger = Slug.standard(ansi: Ansi(true), slugStyle: SlugStyle.arrow);
 
   logger.stdout('Hello world!');
-  logger.trace('message 1');
   await Future.delayed(Duration(milliseconds: 200));
-  logger.trace('message 2');
-  logger.trace('message 3');
 
   var progress = logger.progress('doing some work');
   await Future.delayed(Duration(seconds: 2));
-  progress.finish(showTiming: true);
-  logger.stdout('All ${logger.ansi.emphasized('done')}.');
+  progress.finish(showTiming: true, message: "Completed!");
 }
