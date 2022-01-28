507d5f08013e0794351ea0931b2bcc95cca43651
shan-shaji <abrahamshanshaji@gmail.com>
2022-01-29 00:17:38 +0530

    refactor: removed ansi property

--- lib/src/slug_main.dart
+++ lib/src/slug_main.dart
@@ -7,10 +7,11 @@ import 'package:slug/src/progress/slug_progress.dart';
 /// indeterminate progress.
 abstract class Slug {
   /// Create a normal [Slug];
-  factory Slug({Ansi? ansi, SlugStyle? slugStyle, }) =>
+  factory Slug({
+    Ansi? ansi,
+    SlugStyle? slugStyle,
+  }) =>
       SlugProgress(ansi: ansi, slugStyle: slugStyle);
-
-  Ansi get ansi;
 
   /// Print a standard status message.
   void stdout(String message);
